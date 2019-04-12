27cf2d5f7c806a8f9d811747e1f74799d98e9382
mdb1710 <mdb1710@gmail.com>
2019-04-12 15:37:14 -0400

    work on dogqueue

--- petful-client/src/components/AdoptionSearch.js
+++ petful-client/src/components/AdoptionSearch.js
@@ -2,40 +2,40 @@ import React, { Component } from 'react';
 import CatSearch from './CatSearch';
 import DogSearch from './DogSearch';
 
+import AdoptionContext from '../adoption-context/AdoptionContext';
+
 class AdoptionSearch extends Component {
+    static contextType = AdoptionContext;
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      toggleCat: false,
-      toggleDog: false
+    constructor (props) {
+        super(props) 
+        this.state = {
+            toggle: false
+        }
     }
-  }
 
-  handleOnClick() {
-    this.setState({toggleCat: !this.state.toggleCat})
-  }
-
-  handleOnClickDog() {
-    this.setState({toggleDog: !this.state.toggleDog})
-  }
+    handleOnClick () {
+        this.setState({
+            toggle: true
+        })
+    }
     render () {
+        
         return (
             <div className="adoption-page">
               <h2>Start your search here!</h2>
               <div>
                 <h3>Cats</h3>
-                <button type="submit" onClick= {() => this.handleOnClick()}>Begin Cat Search</button>
+                <button type="submit" onClick={() => this.handleOnClick()}>Begin Cat Search</button>
                 <h3>Dogs</h3>
-                <button type="submit" onClick= {() => this.handleOnClickDog()}>Begin Dog Search</button>
+                <button type="submit" onClick={() => this.handleOnClick()}>Begin Dog Search</button>
               </div>
               
               
               <div className="search-results">
                 <p>Pet Queue would be displayed here</p>
-                <CatSearch toggle={this.state.toggleCat}/>
-                <DogSearch toggle={this.state.toggleDog}/>
+                <CatSearch />
+                <DogSearch />
               </div>
             </div>
         )
--- petful-client/src/components/DogSearch.js
+++ petful-client/src/components/DogSearch.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+
 import './DogSearch.css'
+import Queue from '../helpers/PetQueue';
+
+
 class DogSearch extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            dogs: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(`http://localhost:8000/api/dogs`)
+         .then(res => res.json())
+         .then(dogs => {
+             this.setState({
+                 dogs: dogs
+             })
+         })
+           
+ };
+
+ dogsToQueue(dogs){
+     let dogQueue = new Queue;
+     for (let i = 0; i < dogs.length; i++ ){
+         dogQueue.enqueue(dogs[i]);
+     }
+     return dogQueue;
+ }
+    
     render() {
-      console.log(this.props.toggle)
+      console.log(this.state.dogs)
         return (
             <div className={`dog-results ${this.props.toggle ? 'hidden' : ''}`}>
               <ul>
